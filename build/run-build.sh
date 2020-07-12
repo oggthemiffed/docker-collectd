8d263dff6ac4af6166334820a422544cdaf95aab
David Anderson <david@pibcak.net>
2020-07-12 09:07:27 +0100

    Update run-build.sh

--- build/run-build.sh
+++ build/run-build.sh
@@ -17,7 +17,6 @@ else
       --platform=linux/amd64,linux/386,linux/arm64,linux/arm/v7,linux/arm/v6,linux/ppc64le,linux/s390x \
       -t $DOCKER_REPO:$TAG \
       -t $DOCKER_REPO:$VERSION_TAG \
-      --squash \
       --push \
       .
   exit $?
